003e76ef531f96efbd05474029bc4f6062838299
First Last <salmanashraf853@gmail.com>
2020-07-06 16:13:09 +0100

    geting api

--- src/App.js
+++ src/App.js
@@ -2,23 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const currentYear = 2020
+const country = "US"
+fetch(`https://calendarific.com/api/v2/holidays?&api_key=	22d2b8dd7e5bef1a2e78b2aec1b8cf360e3c529b&country=${country}&year=${currentYear}`).then(e=>{
+    return e.json()
+    .then(e=>{
+        console.log(e)
+    })
+})
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>here we go</h1>
+      <p>open the console</p>
     </div>
   );
 }
